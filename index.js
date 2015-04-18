ad0e361487d6ed76b0d45a288d7e4c5bf036cc4d
Ivan <listepo@ya.ru>
2015-04-18 17:18:21 +0300

    Init

---
+++ index.js
@@ -0,0 +1,40 @@
+var migrate = require('db-migrate');
+
+module.exports = function(sails) {
+
+  return {
+
+    /**
+     * Default configuration
+     *
+     * We do this in a function since the configuration key for
+     * the hook is itself configurable, so we can't just return
+     * an object.
+     */
+    defaults: {
+
+      __configKey__: {
+        // Set autoreload to be active by default
+        active: true
+      }
+    },
+
+    /**
+     * Initialize the hook
+     * @param  {Function} cb Callback for when we're done initializing
+     */
+    initialize: function(cb) {
+
+      if (!sails.config[this.configKey].active) {
+        sails.log.verbose("Autoreload hook deactivated.");
+        return cb();
+      }
+
+      // We're done initializing.
+      return cb();
+
+    },
+
+  };
+
+};
